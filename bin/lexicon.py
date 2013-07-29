66bff644d1abeb47f6ed23b852028ad8136fb243
Xavier Goás Aguililla <xavier@hal.local>
2013-07-29 10:23:08 +0200

    Dictionary

--- bin/lexicon.py
+++ bin/lexicon.py
@@ -3,10 +3,8 @@ from collections import Counter, Iterator
 chars = re.compile('[,\?\!#&_`\.%·; <>]')
 
 class Corpus(Iterator):
-  def __init__(self, file):
-    self.text = open(file)
-    self.freqtable = Counter()
-    self.freqs()
+  def __init__(self, corpus_file):
+    self.text = open(corpus_file)
 
   def __iter__(self):
     return self
@@ -17,41 +15,14 @@ class Corpus(Iterator):
   def tokenise(self, line):
     return chars.split(line.strip().lower().replace("-", ""))
 
-  def freqs(self):
-    logging.info("Building frequency table ...")
-
-    for sentence in self:
-      self.freqtable.update(Counter(sentence))
-      # print(self.freqtable)
-      # input("Press enter")
-
-    self.rewind()
-
-  def rewind(self):
-    self.text.seek(0, 0) # rather avoid missing out on part of the corpus ...
-
-  def most_common(self, number):
-    return self.freqtable.most_common(number).keys()
-
-  def lexicon_size(self):
-    return self.freqtable.keys().size()
-
 class Dictionary:
-  def __init__(self, corpus=None, size=None):
-    self.corpus = corpus
+  def __init__(self, dict_file, size):
     self.indices = {}
-    if corpus != None and size != None:
-      self.build(corpus.most_common(size))
-    self.size = size
-
-  def build(self, freqs):
     i = 0
-    for word in freqs:
-      self.indices[word] = i
+    for word in iter(self.dict_file):
+      if i > size
+      self.indices[word.strip()] = i
       i += 1
-
-  def rebuild(self, size):
-      self.build(self.corpus.most_common(size))
 
   def lookup(self, word):
     return self.indices[word]
